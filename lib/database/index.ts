863c900bfd216f0aff5f7c31d880ee36f5f080ba
LaithMahdi <mahdilaith380@gmail.com>
2023-12-15 23:18:36 +0100

    error correct 1

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI= process.env.MONGOB_URI;
+const MONGODB_URI= process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn:null,promise:null};
 
 export const connectToDatabase = async ()=>{
     if (cached.conn) return cached.conn;
 
-    if(!MONGO_URI) throw new Error('MONGOB_URI is missing');
+    if(!MONGODB_URI) throw new Error('MONGOB_URI is missing');
 
-    cached.promise= cached.promise || mongoose.connect(MONGO_URI,{
+    cached.promise= cached.promise || mongoose.connect(MONGODB_URI,{
         dbName:'evently',
         bufferCommands:false,
     })
